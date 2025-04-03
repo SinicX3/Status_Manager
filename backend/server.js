1ffd331804ecd14763c1976d05c3587ba06938fc
Benoit Thery <thery.benoit@hotmail.fr>
2025-04-03 16:06:20 +0200

    Exercice WebSockets - Base ChatGPT

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let currentColor = "#FFFFFF"; // Stocke la couleur actuelle
+
+io.on("connection", (socket) => {
+  console.log("Un utilisateur s'est connecté");
+
+  // Envoie la couleur actuelle au nouvel utilisateur
+  socket.emit("updateColor", currentColor);
+
+  // Écoute les changements de couleur
+  socket.on("changeColor", (newColor) => {
+    currentColor = newColor; // Met à jour la couleur
+    io.emit("updateColor", newColor); // Diffuse à tous les clients
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Un utilisateur s'est déconnecté");
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Serveur en écoute sur le port 3001");
+});
